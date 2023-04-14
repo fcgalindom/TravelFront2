22af419a74e218260cd23a07da5eb1b5411af99b
fcgalindom <felipecamilo7@gmail.com>
2023-04-14 00:00:36 -0500

    cambios finales

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ function App() {
   <div className="App"  style={{ width: '100vw', height: '100vh' }}>
       <Router>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/mapas">Inicio</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
